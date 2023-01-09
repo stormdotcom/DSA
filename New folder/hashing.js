af9d6b18b0ea3754335a38b2a2ab64d41a69d4ac
Ajmal Nasumudeen <84835379+stormdotcom@users.noreply.github.com>
2023-01-10 00:51:53 +0530

    Update hashing.js

--- New folder/hashing.js
+++ New folder/hashing.js
@@ -1,22 +1,21 @@
 //frequency-of-element-query		
-function findFreq(A, num) {
-			 let freqHM = {};
-			 let count=0
+ function findFreq(A, B) {
+			 let freqHM = new Map();
+			 let count=0;
+			 let result=new Array(B.length).fill(0);
 			 for(let i=0; i<A.length; i++){
-				freqHM[A[i]] =  freqHM[A[i]] ? freqHM[A[i]]+1 : 1;
+				if(freqHM.has(A[i])) {
+				    count = freqHM.get(A[i])
+				    freqHM.set(A[i], count+1);
+				}
+				else {
+				   freqHM.set(A[i], 1);
+				}
 			 }
-			  if(freqHM[num]) {
-			     return freqHM[num]
-			  }
-			  else {
-			      return 0
-			  }
- }
- 
-function findFreqFormQuery(A, B){
-		let result =[];
-		for(let q=0; q<B.length; q++){
-			result.push(findFreq(A, B[q]))
-		}
-		return result;
-	}
+			 for(let j=0; j<B.length; j++){
+			     if(freqHM.has(B[j])) {
+			         result[j]=freqHM.get(B[j])
+			     }
+			 }
+			 return result;
+ } 
