340304def9e4bf3217c52f056f803667586edb31
Ajmal Nasumudeen <ajmaln73@gmail.com>
2023-03-08 13:46:22 +0530

    added message

---
+++ Arrays/ArrayRotation.js
@@ -0,0 +1,2 @@
+//Given an integer array A of size N and an integer B, 
+//you have to return the same array after rotating it B times towards the right.
--- Arrays/ReverseInRange.js
+++ Arrays/ReverseInRange.js
@@ -15,3 +15,5 @@ const reverseInRange=(A=[], B, C)=>{
 }
 
 console.log(reverseInRange([0,1,2,3,4,5,6,7,8,9,10], 3, 8))
+
+export {reverseInRange};
