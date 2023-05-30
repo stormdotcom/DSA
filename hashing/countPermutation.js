d20af0119b3077e5e7752ea325813a336ddc6544
Ajmal Nasumudeen <ajmal.nasumudeen@treeswise.com>
2023-05-30 18:11:44 +0530

    Question modified

--- hashing/countPermutation.js
+++ hashing/countPermutation.js
@@ -8,37 +8,40 @@ const solve = (A, B) => {
     let n = A.length;
     let m = B.length;
     let ans = 0;
+
     for (let i = 0; i < n; i++) {
         const char = A[i];
         hmA.set(char, (hmA.get(char) || 0) + 1);
     }
+
     for (let i = 0; i < n; i++) {
-        const char = A[i];
+        const char = B[i];
         hmB.set(char, (hmB.get(char) || 0) + 1);
     }
-    let l = 0, r = n - 1
-    while (l < m) {
+
+    let l = 0, r = n - 1;
+
+    while (r < m) {
         if (check(hmA, hmB)) {
             ans++;
         }
-        else {
-            hmB.delete(B[l]);
-            l++
-            r++;
-            let char = B[r]
-            hmB.set(char, (hmB.get(char) || 0) + 1);
 
-        }
+        hmB.set(B[l], hmB.get(B[l]) - 1);
+        l++;
+        r++;
+        let char = B[r];
+        hmB.set(char, (hmB.get(char) || 0) + 1);
     }
+
     return ans;
-}
+};
 
 const check = (hmA, hmB) => {
-    for (let c = 97; c <= 122; c++) {
-        const char = String.fromCharCode(c);
-        if (hmA[char] === hmB[char]) {
+    for (let [key, value] of hmA) {
+        if (hmB.get(key) !== value) {
             return false;
         }
     }
+
     return true;
-}
+};
