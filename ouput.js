595f0f859c0113b8a3912828b295f65ee824a6d4
Ajmal Nasumudeen <ajmal.nasumudeen@oceanai.ai>
2024-05-11 21:25:32 +0530

    dfs

---
+++ ouput.js
@@ -0,0 +1,22 @@
+console.log('A');
+
+setTimeout(() => {
+    console.log('B');
+    process.nextTick(() => {
+        console.log('C');
+    });
+    setImmediate(() => {
+        console.log('D');
+    });
+}, 0);
+
+process.nextTick(() => {
+    console.log('E');
+});
+
+setImmediate(() => {
+    console.log('F');
+});
+
+console.log('G');
+// console.log('Node.js version:', process.version);
